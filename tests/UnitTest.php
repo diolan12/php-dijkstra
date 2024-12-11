295dee72f10f8be5f8186db8ac5c3affc05b0e25
Dio Lantief Widoyoko <dio_lantief21@outlook.com>
2024-12-11 18:52:58 +0700

    new testing

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -41,17 +41,18 @@ class UnitTest extends TestCase
         $dijkstra = Dijkstra::instance();
 
         // Add vertices and edges
-        $dijkstra->addVertex('A', ['B' => 3, 'C' => 2]);
-        $dijkstra->addVertex('B', ['A' => 3, 'C' => 1, 'D' => 5]);
+        $dijkstra->addVertex('A', ['B' => 4, 'C' => 2]);
+        $dijkstra->addVertex('B', ['A' => 4, 'C' => 1, 'D' => 4]);
         $dijkstra->addVertex('C', ['A' => 2, 'B' => 1, 'D' => 6]);
-        $dijkstra->addEdge('D', 'B', calcDist(1, 3, 1, 6))->addEdge('D', 'C', 6); // 5
+        $dijkstra->addEdge('D', 'B', 4)->addEdge('D', 'C', 6);
 
         $paths = $dijkstra->findShortestPath('A', 'D');
 
-        $this->assertEquals(3, count($paths), 'Shortest path should be 3 array');
+        $this->assertEquals(4, count($paths), 'Shortest path should be 4 array');
         $this->assertEquals('A', $paths[0], 'First node should be A');
-        $this->assertEquals('C', $paths[1], 'First node should be C');
-        $this->assertEquals('D', $paths[2], 'First node should be D');
+        $this->assertEquals('C', $paths[1], 'Second node should be C');
+        $this->assertEquals('B', $paths[2], 'Third node should be B');
+        $this->assertEquals('D', $paths[3], 'Fourth node should be D');
     }
 
     /**
@@ -101,23 +102,27 @@ class UnitTest extends TestCase
     public function testNumericalNames()
     {
         $graph = [
-            '1' => [
-                '2' => 3,
-                '3' => 2
+            1 => [
+                2 => 7,
+                3 => 9,
+                6 => 14
             ]
         ];
         $dijkstra = Dijkstra::instance($graph);
 
-        $dijkstra->addVertex('2', ['1' => 3, '3' => 1, '4' => 5]);
-        $dijkstra->addVertex('3', ['1' => 2, '2' => 1, '4' => 6]);
-        $dijkstra->addEdge('4', '2', calcDist(1, 3, 1, 6))->addEdge('4', '3', 6); // 5
+        $dijkstra->addVertex(2, [1 => 7, 3 => 10, 4 => 15]);
+        $dijkstra->addVertex(3, [1 => 9, 2 => 10, 4 => 11, 6 => 2]);
+        $dijkstra->addEdge(4, 2, 16)->addEdge(4, 3, 11)->addEdge(4, 5, 6);
+        $dijkstra->addEdge(5, 4, 6)->addEdge(5, 6, 9);
+        $dijkstra->addEdge(6, 1, 14)->addEdge(6, 3, 2)->addEdge(6, 5, 9);
 
-        $paths = $dijkstra->findShortestPath('1', '4');
+        $paths = $dijkstra->findShortestPath(1, 5);
 
-        $this->assertEquals(3, count($paths), 'Node should be 3 array');
-        $this->assertEquals('1', $paths[0], 'First node should be 1');
-        $this->assertEquals('3', $paths[1], 'First node should be 3');
-        $this->assertEquals('4', $paths[2], 'First node should be 4');
+        $this->assertEquals(4, count($paths), 'Node should be 3 array');
+        $this->assertEquals(1, $paths[0], 'First node should be 1');
+        $this->assertEquals(3, $paths[1], 'First node should be 3');
+        $this->assertEquals(6, $paths[2], 'First node should be 6');
+        $this->assertEquals(5, $paths[3], 'First node should be 5');
     }
 
     /**
